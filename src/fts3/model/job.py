14ee45831823af95cc57a631ff3ec3dd22073621
ayllon <a.alvarezayllon@gmail.com>
2013-03-22 09:35:25 +0100

    [t_job] source and dest are unused

--- src/fts3/model/job.py
+++ src/fts3/model/job.py
@@ -16,8 +16,6 @@ class Job(Base):
 	cancel_job               = Column(String(1))
 	job_params               = Column(String(255))
 	submit_host              = Column(String(255))
-	source                   = Column(String(255))
-	dest                     = Column(String(255))
 	user_dn                  = Column(String(1024))
 	agent_dn                 = Column(String(1024))
 	user_cred                = Column(String(255))
