17f8d7ed3d0f3b326a1d8fdc37f6a4bfe1a4c86c
Martin Hellmich <mhellmic@gmail.com>
2013-06-05 16:51:54 +0200

    fixed getting the default values from the DEFAULT section

--- src/fts3/cli/base.py
+++ src/fts3/cli/base.py
@@ -1,10 +1,20 @@
-from ConfigParser import ConfigParser
+from ConfigParser import SafeConfigParser
 from optparse import OptionParser
 import logging
 import os
 
 CONFIG_FILENAMES = ['/etc/fts3/fts3client.cfg',
 										os.path.expanduser('~/.fts3client.cfg')]
+
+CONFIG_DEFAULTSECTION = 'Main'
+CONFIG_DEFAULTS =
+									{
+										'verbose': 'false',
+										'endpoint': None,
+										'json': 'false',
+										'ukey': None,
+										'ucert': None
+									}
 
 class Base(object):
 	
@@ -16,29 +26,27 @@ class Base(object):
 		if extra_args:
 			usage = "usage: %prog [options] " + extra_args
 
-		config = ConfigParser(defaults=
-				{
-					'verbose': 'false',
-					'endpoint': None,
-					'json': 'false',
-					'ukey': None,
-					'ucert': None
-				}
-			)
+		config = SafeConfigParser(defaults=CONFIG_DEFAULTS)
+
+		section = CONFIG_DEFAULTSECTION
 		config.read(CONFIG_FILENAMES)
+
+		# manually set the section in edge cases
+		if not config.has_section('Main'):
+			section = 'DEFAULT'
 		
 		self.optParser = OptionParser(usage = usage)
 		
 		self.optParser.add_option('-v', '--verbose', dest = 'verbose', action = 'store_true',
-								  help = 'verbose output.', default=config.getboolean('Main', 'verbose'))
+								  help = 'verbose output.', default=config.getboolean(section, 'verbose'))
 		self.optParser.add_option('-s', '--endpoint', dest = 'endpoint',
-								  help = 'FTS3 REST endpoint.', default=config.get('Main', 'endpoint'))
+								  help = 'FTS3 REST endpoint.', default=config.get(section, 'endpoint'))
 		self.optParser.add_option('-j', dest = 'json', action = 'store_true',
 								  help = 'print the output in JSON format.',
-									default=config.get('Main', 'json'))
+									default=config.get(section, 'json'))
 		self.optParser.add_option('--key', dest = 'ukey',
 								  help = 'the user certificate private key.',
-									default=config.get('Main', 'ukey'))
+									default=config.get(section, 'ukey'))
 		self.optParser.add_option('--cert', dest = 'ucert',
 								  help = 'the user certificate.',
-									default=config.get('Main', 'ucert'))
+									default=config.get(section, 'ucert'))
