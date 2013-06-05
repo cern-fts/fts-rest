84611fe7659347da93366a11d9f3e685e0523848
Martin Hellmich <mhellmic@gmail.com>
2013-06-05 17:06:21 +0200

    manually support parameters that can be None, fix

--- src/fts3/cli/base.py
+++ src/fts3/cli/base.py
@@ -9,10 +9,10 @@ CONFIG_FILENAMES = ['/etc/fts3/fts3client.cfg',
 CONFIG_DEFAULTSECTION = 'Main'
 CONFIG_DEFAULTS = {
 										'verbose': 'false',
-										'endpoint': None,
+										'endpoint': 'None',
 										'json': 'false',
-										'ukey': None,
-										'ucert': None
+										'ukey': 'None',
+										'ucert': 'None'
 									}
 
 class Base(object):
@@ -33,19 +33,28 @@ class Base(object):
 		# manually set the section in edge cases
 		if not config.has_section('Main'):
 			section = 'DEFAULT'
+
+		# manually get values for which we need to support None
+		opt_endpoint = config.get(section, 'endpoint')
+		if opt_endpoint == 'None':
+			opt_endpoint = None
+		opt_ukey = config.get(section, 'ukey')
+		if opt_ukey == 'None':
+			opt_ukey = None
+		opt_ucert = config.get(section, 'ucert')
+		if opt_ucert == 'None':
+			opt_ucert = None
 		
 		self.optParser = OptionParser(usage = usage)
 		
 		self.optParser.add_option('-v', '--verbose', dest = 'verbose', action = 'store_true',
 								  help = 'verbose output.', default=config.getboolean(section, 'verbose'))
 		self.optParser.add_option('-s', '--endpoint', dest = 'endpoint',
-								  help = 'FTS3 REST endpoint.', default=config.get(section, 'endpoint'))
+								  help = 'FTS3 REST endpoint.', default=opt_endpoint)
 		self.optParser.add_option('-j', dest = 'json', action = 'store_true',
 								  help = 'print the output in JSON format.',
 									default=config.getboolean(section, 'json'))
 		self.optParser.add_option('--key', dest = 'ukey',
-								  help = 'the user certificate private key.',
-									default=config.get(section, 'ukey'))
+								  help = 'the user certificate private key.', default=opt_ukey)
 		self.optParser.add_option('--cert', dest = 'ucert',
-								  help = 'the user certificate.',
-									default=config.get(section, 'ucert'))
+								  help = 'the user certificate.', default=opt_ucert)
