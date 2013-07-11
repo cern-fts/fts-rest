0f60988e900399444cc63d1b36c40a60707f1062
ayllon <a.alvarezayllon@gmail.com>
2013-07-11 14:10:21 +0200

    Fixed a test that wasn't running due to wrong indent.

--- src/fts3rest/fts3rest/tests/unit/test_credentials.py
+++ src/fts3rest/fts3rest/tests/unit/test_credentials.py
@@ -13,7 +13,7 @@ class TestUserCredentials(unittest.TestCase):
     
     ROLES = {
              'public': {'transfer': 'vo', 'deleg': ''},
-             'admin': {'*': 'all'}
+             'admin': {'config': 'all'}
             }
     
     def test_basic_ssl(self):
@@ -53,16 +53,16 @@ class TestUserCredentials(unittest.TestCase):
         self.assertEqual(fts3auth.NONE,    creds.getGrantedLevelFor(fts3auth.CONFIG))
 
 
-	def test_roles(self):
-		env = {}
-		env['GRST_CRED_AURI_0'] = 'dn:' + TestUserCredentials.DN
-		env['GRST_CRED_AURI_1'] = 'fqan:' + TestUserCredentials.FQANS[0]
-		env['GRST_CRED_AURI_2'] = 'fqan:' + TestUserCredentials.FQANS[1]
-		env['GRST_CRED_AURI_3'] = 'fqan:' + TestUserCredentials.FQANS[2]
-		env['GRST_CRED_AURI_4'] = 'fqan:' + TestUserCredentials.FQANS[3]
-		
-		creds = UserCredentials(env, TestUserCredentials.ROLES)
-		
-		self.assertEqual(fts3auth.ALL, creds.getGrantedLevelFor(fts3auth.CONFIG))
-		self.assertEqual(fts3auth.ALL, creds.getGrantedLevelFor(fts3auth.TRANSFER))
-		self.assertEqual(fts3auth.ALL, creds.getGrantedLevelFor(fts3auth.DELEGATION))
+    def test_roles(self):
+        env = {}
+        env['GRST_CRED_AURI_0'] = 'dn:' + TestUserCredentials.DN
+        env['GRST_CRED_AURI_1'] = 'fqan:' + TestUserCredentials.FQANS[0]
+        env['GRST_CRED_AURI_2'] = 'fqan:' + TestUserCredentials.FQANS[1]
+        env['GRST_CRED_AURI_3'] = 'fqan:' + TestUserCredentials.FQANS[2]
+        env['GRST_CRED_AURI_4'] = 'fqan:' + TestUserCredentials.FQANS[3]
+        
+        creds = fts3auth.UserCredentials(env, TestUserCredentials.ROLES)
+        
+        self.assertEqual(fts3auth.ALL,     creds.getGrantedLevelFor(fts3auth.CONFIG))
+        self.assertEqual(fts3auth.VO,      creds.getGrantedLevelFor(fts3auth.TRANSFER))
+        self.assertEqual(fts3auth.PRIVATE, creds.getGrantedLevelFor(fts3auth.DELEGATION))
