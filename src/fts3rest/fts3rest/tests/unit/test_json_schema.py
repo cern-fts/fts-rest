5c9fad4dce7837aa43be571262c95b7e3d3d0103
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2014-02-13 14:18:01 +0100

    Fix test test_json_schema

--- src/fts3rest/fts3rest/tests/unit/test_json_schema.py
+++ src/fts3rest/fts3rest/tests/unit/test_json_schema.py
@@ -1,4 +1,4 @@
-from fts3rest.controllers.api import _schema
+from fts3rest.lib.api import SubmitSchema
 import unittest
 import jsonschema
 
@@ -31,7 +31,7 @@ class TestJsonSchema(unittest.TestCase):
                         "gridftp": None
                       }
                     }
-        self.schema = _schema()
+        self.schema = SubmitSchema
 
 
     def test_validation(self):
