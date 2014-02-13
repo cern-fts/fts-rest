c2d440abd9807f47847c623b91d2aef1d668286c
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2014-02-13 09:34:07 +0100

    Sorting API documentation

--- src/fts3rest/fts3rest/controllers/api.py
+++ src/fts3rest/fts3rest/controllers/api.py
@@ -1,3 +1,4 @@
+from fts3rest.lib.api import doc
 from fts3rest.lib.base import BaseController, Session
 from fts3rest.lib.helpers import jsonify
 from fts3rest.lib import api
@@ -6,20 +7,33 @@ from pylons.controllers.util import abort
 
 
 class ApiController(BaseController):
+    """
+    API documentation
+    """
     
     def __init__(self):
-        self.resources, self.apis, self.models = api.introspect() 
+        self.resources, self.apis, self.models = api.introspect()
+        self.resources.sort(key = lambda r: r['path'])
+        for r in self.apis.values():
+            r.sort(key = lambda a: a['path'])
      
     @jsonify
     def submit_schema(self):
         """
-        Return the json-schema for the submission operation, so it can
-        be used to validate
+        Json-schema for the submission operation
+        
+        This can be used to validate the submission. For instance, in Python,
+        jsonschema.validate 
         """
         return api.SubmitSchema
 
     @jsonify
     def api_docs(self):
+        """
+        Auto-generated API documentation
+        
+        Compatible with Swagger-UI
+        """
         return {
             'swaggerVersion': '1.2',
             'apis': self.resources,
@@ -32,8 +46,12 @@ class ApiController(BaseController):
             }
         }
 
+    @doc.response(404, 'The resource can not be found')
     @jsonify
     def resource_doc(self, resource):
+        """
+        Auto-generated API documentation for a specific resource
+        """
         resource_path = '/' + resource
         if resource_path not in self.apis:
             abort(404, 'API not found: ' + resource) 
