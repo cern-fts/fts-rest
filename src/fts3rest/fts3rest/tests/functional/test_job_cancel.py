ebfde0f7f2662ba1f8069918c8ebd4ef87c792f4
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2014-03-11 10:35:13 +0100

    Added two tests to test_job_cancel.py

--- src/fts3rest/fts3rest/tests/functional/test_job_cancel.py
+++ src/fts3rest/fts3rest/tests/functional/test_job_cancel.py
@@ -10,22 +10,26 @@ class TestJobCancel(TestController):
     Tests for the job cancellation
     """
 
-    def _submit(self):
+    def _submit(self, count=1):
         """
         Submit a valid job
         """
         self.setup_gridsite_environment()
         self.push_delegation()
 
-        job = {
-            'files': [{
-                'sources': ['root://source.es/file'],
-                'destinations': ['root://dest.ch/file'],
+        files = []
+        for i in xrange(count):
+            files.append({
+                'sources': ['root://source.es/file%d' % i],
+                'destinations': ['root://dest.ch/file%d' % i],
                 'selection_strategy': 'orderly',
                 'checksum': 'adler32:1234',
                 'filesize': 1024,
                 'metadata': {'mykey': 'myvalue'},
-            }],
+            })
+
+        job = {
+            'files': files,
             'params': {'overwrite': True, 'verify_checksum': True}
         }
 
@@ -55,3 +59,62 @@ class TestJobCancel(TestController):
         self.assertEqual(job.job_state, 'CANCELED')
         for f in job.files:
             self.assertEqual(f.file_state, 'CANCELED')
+
+    def test_cancel_terminal(self):
+        """
+        Cancel a job with files in terminal state
+        """
+        job_id = self._submit()
+
+        job = Session.query(Job).get(job_id)
+        job.job_state = 'FINISHED'
+        for f in job.files:
+            f.file_state = 'FINISHED'
+        Session.merge(job)
+        Session.commit()
+
+        answer = self.app.delete(url="/jobs/%s" % job_id,
+                                 status=200)
+        job = json.loads(answer.body)
+
+        self.assertEqual(job['job_id'], job_id)
+        self.assertEqual(job['job_state'], 'FINISHED')
+        self.assertNotEqual(job['reason'], 'Job canceled by the user')
+
+        # Is it in the database?
+        job = Session.query(Job).get(job_id)
+        self.assertEqual(job.job_state, 'FINISHED')
+        for f in job.files:
+            self.assertEqual(f.file_state, 'FINISHED')
+
+    def test_cancel_some_terminal(self):
+        """
+        Cancel a job with some files in terminal state
+        """
+        job_id = self._submit(10)
+
+        job = Session.query(Job).get(job_id)
+        job.job_state = 'ACTIVE'
+        for f in job.files:
+            if f.file_id % 2 == 0:
+                f.file_state = 'FINISHED'
+        Session.merge(job)
+        Session.commit()
+
+        answer = self.app.delete(url="/jobs/%s" % job_id,
+                                 status=200)
+        job = json.loads(answer.body)
+
+        self.assertEqual(job['job_id'], job_id)
+        self.assertEqual(job['job_state'], 'CANCELED')
+        self.assertEqual(job['reason'], 'Job canceled by the user')
+
+        # Is it in the database?
+        job = Session.query(Job).get(job_id)
+        self.assertEqual(job.job_state, 'CANCELED')
+        for f in job.files:
+            if f.file_id % 2 == 0:
+                self.assertEqual(f.file_state, 'FINISHED')
+                self.assertNotEqual(f.reason, 'Job canceled by the user')
+            else:
+                self.assertEqual(f.file_state, 'CANCELED')
