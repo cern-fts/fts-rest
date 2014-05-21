f0fd3086a0ca22c7587d6b7d3a1b2b2831a98860
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2014-05-21 14:47:21 +0200

    Context.post_json improvements

--- src/fts3/rest/client/context.py
+++ src/fts3/rest/client/context.py
@@ -1,14 +1,14 @@
 #   Copyright notice:
 #   Copyright  Members of the EMI Collaboration, 2013.
-# 
+#
 #   See www.eu-emi.eu for details on the copyright holders
-# 
+#
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
-# 
+#
 #       http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -144,7 +144,9 @@ class Context(object):
                                       "%s/%s" % (self.endpoint, path))
 
     def post_json(self, path, body):
+        if not isinstance(body, str) and not isinstance(body, unicode):
+            body = json.dumps(body)
         return self._requester.method('POST',
                                       "%s/%s" % (self.endpoint, path),
                                       body,
-                                      headers={'Content-Type': 'text/json'})
+                                      headers={'Content-Type': 'application/json'})
