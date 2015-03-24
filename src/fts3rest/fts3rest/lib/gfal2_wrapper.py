60437c46c5ddad4de5b3a52e290eef6d48f33593
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-03-24 11:33:43 +0100

    Make running tests easier

--- src/fts3rest/fts3rest/lib/gfal2_wrapper.py
+++ src/fts3rest/fts3rest/lib/gfal2_wrapper.py
@@ -1,14 +1,14 @@
 #   Copyright notice:
 #   Copyright  Members of the EMI Collaboration, 2013.
-# 
+#
 #   See www.eu-emi.eu for details on the copyright holders
-# 
+#
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
-# 
+#
 #       http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,10 +21,11 @@ import os
 import signal
 from StringIO import StringIO
 
-import gfal2
-
-
-context_type = gfal2.creat_context
+try:
+    import gfal2
+    context_type = gfal2.creat_context
+except:
+    context_type = None
 
 
 class Gfal2Error(Exception):
@@ -88,7 +89,9 @@ class Gfal2Wrapper(object):
         os.environ['X509_USER_PROXY'] = self.proxy.name
         exit_code = 0
         try:
-            ctx = gfal2.creat_context()
+            if context_type is None:
+                raise RuntimeError('Could not load the gfal2 python module')
+            ctx = context_type()
             result = self.method(ctx, *args, **kwargs)
             pipe.write(json.dumps(result))
         except gfal2.GError, e:
