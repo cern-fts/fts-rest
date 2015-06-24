0e97c8843f29a7fdaac8565c01f9f8464b094c7e
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-06-24 11:19:39 +0200

    Retab websetup.py

--- src/fts3rest/fts3rest/websetup.py
+++ src/fts3rest/fts3rest/websetup.py
@@ -27,21 +27,21 @@ from fts3rest.model.meta import Session, Base
 log = logging.getLogger(__name__)
 
 def setup_app(command, conf, vars):
-	"""Place any commands to setup fts3rest here"""
-	cfgFilename = os.path.split(conf.filename)[-1]
-   
-	# Don't reload the app if it was loaded under the testing environment
-	if not pylons.test.pylonsapp:
-		load_environment(conf.global_conf, conf.local_conf)
-		
-	# Connect
-	Base.metadata.bind = Session.bind
+    """Place any commands to setup fts3rest here"""
+    cfgFilename = os.path.split(conf.filename)[-1]
 
-	# Drop tables if running tests
-	if cfgFilename == 'test.ini':
-		log.info("Dropping existing tables for the testing environment")
-		Base.metadata.drop_all(checkfirst = True)
+    # Don't reload the app if it was loaded under the testing environment
+    if not pylons.test.pylonsapp:
+        load_environment(conf.global_conf, conf.local_conf)
 
-	# Create the tables if they don't already exist
-	log.info("Creating tables if they do not exist")
-	Base.metadata.create_all(checkfirst = True)
+    # Connect
+    Base.metadata.bind = Session.bind
+
+    # Drop tables if running tests
+    if cfgFilename == 'test.ini':
+        log.info("Dropping existing tables for the testing environment")
+        Base.metadata.drop_all(checkfirst = True)
+
+    # Create the tables if they don't already exist
+    log.info("Creating tables if they do not exist")
+    Base.metadata.create_all(checkfirst = True)
