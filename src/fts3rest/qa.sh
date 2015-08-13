36854184c3c05bdf45365e49546bb684ecf164f2
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-08-13 16:12:31 +0200

    Update setuptools

--- src/fts3rest/qa.sh
+++ src/fts3rest/qa.sh
@@ -43,7 +43,7 @@ fi
 
 pip install --upgrade \
     WebTest==1.4.3 WebOb==1.1.1 Pylons==1.0 \
-    nose==1.2 nose-cov==1.2 \
+    nose==1.2 nose-cov==1.2 "setuptools>=17.1" \
     sqlalchemy M2Crypto m2ext python-dateutil requests jsonschema mock
 if [ $? -ne 0 ]; then
     echo "pip install failed"
