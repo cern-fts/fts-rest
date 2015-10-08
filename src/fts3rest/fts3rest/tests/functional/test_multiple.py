9324b44540744a90ff3f4bec51e8f45cc162a7fb
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-10-08 18:19:11 +0200

    Improve test_multiple

--- src/fts3rest/fts3rest/tests/functional/test_multiple.py
+++ src/fts3rest/fts3rest/tests/functional/test_multiple.py
@@ -47,6 +47,7 @@ class TestMultiple(TestController):
                     'selection_strategy': 'orderly',
                     'checksum': 'adler32:1234',
                     'filesize': 1024,
+                    'activity': 'something something',
                     'metadata': {'mykey': 'myvalue'},
                 }
             ],
@@ -70,22 +71,84 @@ class TestMultiple(TestController):
         self.assertEqual(db_job.files[0].file_index, 0)
         self.assertEqual(db_job.files[0].source_surl, 'http://source.es:8446/file')
         self.assertEqual(db_job.files[0].dest_surl, 'http://dest.ch:8447/file')
+        self.assertEqual(db_job.files[0].activity, 'something something')
         self.assertEqual(db_job.files[0].file_metadata['mykey'], 'myvalue')
+        self.assertEqual(db_job.files[0].file_state, 'SUBMITTED')
 
         self.assertEqual(db_job.files[1].file_index, 0)
         self.assertEqual(db_job.files[1].source_surl, 'http://source.es:8446/file')
         self.assertEqual(db_job.files[1].dest_surl, 'root://dest.ch/file')
+        self.assertEqual(db_job.files[1].activity, 'something something')
         self.assertEqual(db_job.files[1].file_metadata['mykey'], 'myvalue')
+        self.assertEqual(db_job.files[1].file_state, 'NOT_USED')
 
         self.assertEqual(db_job.files[2].file_index, 0)
         self.assertEqual(db_job.files[2].source_surl, 'root://source.es/file')
         self.assertEqual(db_job.files[2].dest_surl, 'http://dest.ch:8447/file')
+        self.assertEqual(db_job.files[2].activity, 'something something')
         self.assertEqual(db_job.files[2].file_metadata['mykey'], 'myvalue')
+        self.assertEqual(db_job.files[2].file_state, 'NOT_USED')
 
         self.assertEqual(db_job.files[3].file_index, 0)
         self.assertEqual(db_job.files[3].source_surl, 'root://source.es/file')
         self.assertEqual(db_job.files[3].dest_surl, 'root://dest.ch/file')
+        self.assertEqual(db_job.files[3].activity, 'something something')
         self.assertEqual(db_job.files[3].file_metadata['mykey'], 'myvalue')
+        self.assertEqual(db_job.files[3].file_state, 'NOT_USED')
+
+        # Same file index, same hashed id
+        uniq_hashes = set(map(lambda f: f.hashed_id, db_job.files))
+        self.assertEqual(len(uniq_hashes), 1)
+
+    def test_submit_with_alternatives2(self):
+        """
+        Submit one transfer with multiple sources and one destinations.
+        It must be treated as a transfer with alternatives
+        """
+        self.setup_gridsite_environment()
+        self.push_delegation()
+
+        job = {
+            'files': [
+                {
+                    'sources': ['http://source.es/file', 'http://source.fr/file'],
+                    'destinations': ['http://dest.ch/file'],
+                    'selection_strategy': 'orderly',
+                    'checksum': 'adler32:1234',
+                    'filesize': 1024,
+                    'activity': 'something something',
+                    'metadata': {'mykey': 'myvalue'},
+                }
+            ],
+            'params': {'overwrite': True}
+        }
+
+        answer = self.app.post(url="/jobs",
+                               content_type='application/json',
+                               params=json.dumps(job),
+                               status=200)
+
+        # Validate job in the database
+        job_id = json.loads(answer.body)['job_id']
+        db_job = Session.query(Job).get(job_id)
+
+        self.assertEqual(db_job.reuse_job, 'R')
+
+        self.assertEqual(len(db_job.files), 2)
+
+        self.assertEqual(db_job.files[0].file_index, 0)
+        self.assertEqual(db_job.files[0].source_surl, 'http://source.es/file')
+        self.assertEqual(db_job.files[0].dest_surl, 'http://dest.ch/file')
+        self.assertEqual(db_job.files[0].activity, 'something something')
+        self.assertEqual(db_job.files[0].file_metadata['mykey'], 'myvalue')
+        self.assertEqual(db_job.files[0].file_state, 'SUBMITTED')
+
+        self.assertEqual(db_job.files[1].file_index, 0)
+        self.assertEqual(db_job.files[1].source_surl, 'http://source.fr/file')
+        self.assertEqual(db_job.files[1].dest_surl, 'http://dest.ch/file')
+        self.assertEqual(db_job.files[1].activity, 'something something')
+        self.assertEqual(db_job.files[1].file_metadata['mykey'], 'myvalue')
+        self.assertEqual(db_job.files[1].file_state, 'NOT_USED')
 
         # Same file index, same hashed id
         uniq_hashes = set(map(lambda f: f.hashed_id, db_job.files))
