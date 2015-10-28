1b724af2fe592775762e4f218085812faa86627e
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2015-10-28 16:18:53 +0100

    m2crypto > 0.22.3 from pypi breaks

--- src/fts3rest/qa.sh
+++ src/fts3rest/qa.sh
@@ -34,7 +34,7 @@ if [ $? -eq 0 ]; then
 	echo "Ugly workaround for M2Crypto in RHEL5/6"
 	rm -rf "${VIRTUALENV}/tmp/build/M2Crypto"
 	export SWIG_FEATURES="-includeall -D__`uname -m`__"
-	pip install --build "${VIRTUALENV}/tmp/build" "M2Crypto>=0.16"
+	pip install --build "${VIRTUALENV}/tmp/build" "M2Crypto==0.22.3"
 	if [ $? -ne 0 ]; then
 		echo "Workaround for M2Crypto failed"
 		exit 1
@@ -47,7 +47,7 @@ pip install --upgrade setuptools==17.1
 pip install --upgrade \
     WebTest==1.4.3 WebOb==1.1.1 Pylons==1.0 \
     nose==1.2 nose-cov==1.2 \
-    sqlalchemy M2Crypto m2ext python-dateutil requests jsonschema mock funcsigs
+    sqlalchemy M2Crypto==0.22.3 m2ext python-dateutil requests jsonschema mock funcsigs
 if [ $? -ne 0 ]; then
     echo "pip install failed"
     exit 1
