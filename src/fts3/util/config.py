b2eb3f53b96191f2a07f23db4a1f69c37e76f8c5
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2016-02-26 09:59:27 +0100

    FTS-497: Load full config file

--- src/fts3/util/config.py
+++ src/fts3/util/config.py
@@ -26,6 +26,7 @@ def fts3_config_load(path='/etc/fts3/fts3config'):
     Read the configuration from the FTS3 configuration file and
     pass it to the Pylons configuration
     """
+    fts3cfg = {}
 
     # Dirty workaround: ConfigParser doesn't like files without
     # headers, so fake one (since FTS3 config file doesn't have a
@@ -34,60 +35,61 @@ def fts3_config_load(path='/etc/fts3/fts3config'):
     io = StringIO(content)
 
     parser = ConfigParser()
+    parser.optionxform = str
     parser.readfp(io)
 
-    db_type = parser.get('fts3', 'DbType')
-    db_user = parser.get('fts3', 'DbUserName')
-    db_pass = parser.get('fts3', 'DbPassword')
-    db_conn = parser.get('fts3', 'DbConnectString')
+    # Map all options
+    for name, value in parser.items('fts3'):
+        fts3cfg['fts3.' + name] = value
 
-    if len(db_conn) > 0 and db_conn[0] == '"' and db_conn[-1] == '"':
-        db_conn = db_conn[1:-1]
+    # AuthorizedVO is an array
+    fts3cfg['fts3.AuthorizedVO'] = fts3cfg['fts3.AuthorizedVO'].split(';')
 
-    fts3cfg = {}
+    # DbType always lowercase
+    fts3cfg['fts3.DbType'] = fts3cfg['fts3.DbType'].lower()
 
-    if db_type.lower() == 'mysql':
-        fts3cfg['sqlalchemy.url'] = "mysql://%s:%s@%s" % (db_user, quote_plus(db_pass),
-                                                          db_conn)
-    elif db_type.lower() == 'sqlite':
-        if db_conn and not db_conn.startswith('/'):
-            db_conn = os.path.abspath(db_conn)
-        fts3cfg['sqlalchemy.url'] = "sqlite:///%s" % db_conn
-    elif db_type.lower() == 'oracle':
-        fts3cfg['sqlalchemy.url'] = "oracle://%s:%s@%s" % (db_user, quote_plus(db_pass),
-                                                           db_conn)
-    else:
-        raise ValueError("Database type '%s' is not recognized" % db_type)
-
-    fts3cfg['fts3.Db.Type']             = db_type
-    fts3cfg['fts3.Db.Username']         = db_user
-    fts3cfg['fts3.Db.Password']         = db_pass
-    fts3cfg['fts3.Db.ConnectionString'] = db_conn
-
-    authorized_vos = parser.get('fts3', 'AuthorizedVO').split(';')
-    fts3cfg['fts3.AuthorizedVO'] = authorized_vos
-
-    fts3cfg['fts3.Roles'] = {}
-    for role in parser.options('roles'):
-        granted_array = parser.get('roles', role).split(';')
-        for granted in granted_array:
-            if granted.find(':') == -1:
-               level = ''
-               operation = granted
-            else:
-               (level, operation) = granted.split(':')
-            if role not in fts3cfg['fts3.Roles']:
-                fts3cfg['fts3.Roles'][role] = {}
-            fts3cfg['fts3.Roles'][role][operation] = level
-
+    # Optimizer is a boolean
     try:
         fts3cfg['fts3.Optimizer'] = parser.getboolean('fts3', 'Optimizer')
     except NoOptionError:
         fts3cfg['fts3.Optimizer'] = True
 
-    try:
-        fts3cfg['fts3.SiteName'] = parser.get('fts3', 'SiteName')
-    except:
+    # Put something in SiteName
+    if 'fts3.SiteName' not in fts3cfg:
         fts3cfg['fts3.SiteName'] = ''
+
+    # Prepare database connection url
+    if len(fts3cfg['fts3.DbConnectString']) > 0:
+        if fts3cfg['fts3.DbConnectString'][0] == '"' and fts3cfg['fts3.DbConnectString'][-1] == '"':
+            fts3cfg['fts3.DbConnectString'] = fts3cfg['fts3.DbConnectString'][1:-1]
+
+    if fts3cfg['fts3.DbType'] == 'mysql':
+        fts3cfg['sqlalchemy.url'] = "mysql://%s:%s@%s" % (fts3cfg['fts3.DbUserName'],
+                                                          quote_plus(fts3cfg['fts3.DbPassword']),
+                                                          fts3cfg['fts3.DbConnectString'])
+    elif fts3cfg['fts3.DbType'] == 'sqlite':
+        if fts3cfg['fts3.DbConnectString'] and not fts3cfg['fts3.DbConnectString'].startswith('/'):
+            fts3cfg['fts3.DbConnectString'] = os.path.abspath(fts3cfg['fts3.DbConnectString'])
+        fts3cfg['sqlalchemy.url'] = "sqlite:///%s" % fts3cfg['fts3.DbConnectString']
+    elif fts3cfg['fts3.DbType'] == 'oracle':
+        fts3cfg['sqlalchemy.url'] = "oracle://%s:%s@%s" % (fts3cfg['fts3.DbUserName'],
+                                                           quote_plus(fts3cfg['fts3.DbPassword']),
+                                                           fts3cfg['fts3.DbConnectString'])
+    else:
+        raise ValueError("Database type '%s' is not recognized" % fts3cfg['fts3.DbType'])
+
+    # Initialize roles
+    fts3cfg['fts3.Roles'] = {}
+    for role in parser.options('roles'):
+        granted_array = parser.get('roles', role).split(';')
+        for granted in granted_array:
+            if granted.find(':') == -1:
+                level = ''
+                operation = granted
+            else:
+                (level, operation) = granted.split(':')
+            if role.lower() not in fts3cfg['fts3.Roles']:
+                fts3cfg['fts3.Roles'][role.lower()] = {}
+            fts3cfg['fts3.Roles'][role.lower()][operation.lower()] = level.lower()
 
     return fts3cfg
