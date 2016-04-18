6e600e814158f2aabb5f1e6ad31cab00addc4011
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2016-04-18 11:10:06 +0200

    Retab http.py

--- src/fts3rest/fts3rest/lib/middleware/fts3auth/methods/http.py
+++ src/fts3rest/fts3rest/lib/middleware/fts3auth/methods/http.py
@@ -48,12 +48,12 @@ def do_authentication(credentials, env):
     try:
         cert = b64decode(cred['cert'])
         sign = b64decode(cred['sign'])
-	proxy = None
-	try:
-		proxy =  b64decode(cred['prx'])
-		log.info("found a proxy on the request")
-	except:
-		pass
+        proxy = None
+        try:
+                proxy =  b64decode(cred['prx'])
+                log.info("found a proxy on the request")
+        except:
+                pass
         ts = dateutil.parser.parse(cred['ts']).strftime('%s')
     except (TypeError, ValueError):
         log.info("Cannot decode certificate, signature or timestamp")
@@ -64,17 +64,17 @@ def do_authentication(credentials, env):
         log.info("Authorization has expired by " + str(td) + " seconds")
         raise InvalidCredentials("Authorization has expired by " + str(td) + " seconds")
     if proxy:
-	x509 = X509.load_cert_string(proxy, X509.FORMAT_DER)
-	fileCertString = X509.load_cert_string(cert, X509.FORMAT_DER)
-	#print the cert DN
-	certDN =  '/' + '/'.join(fileCertString.get_subject().as_text().split(', '))
+        x509 = X509.load_cert_string(proxy, X509.FORMAT_DER)
+        fileCertString = X509.load_cert_string(cert, X509.FORMAT_DER)
+        #print the cert DN
+        certDN =  '/' + '/'.join(fileCertString.get_subject().as_text().split(', '))
         proxyDN = '/' + '/'.join(x509.get_subject().as_text().split(', '))
-	log.info("cert DN: "+ certDN)
-	chain_pem =fileCertString.as_pem()
- 	chain_pem += x509.as_pem()
+        log.info("cert DN: "+ certDN)
+        chain_pem =fileCertString.as_pem()
+        chain_pem += x509.as_pem()
         chain = X509.load_cert_string(chain_pem)
     else:
-	x509 = X509.load_cert_string(cert, X509.FORMAT_DER)
+        x509 = X509.load_cert_string(cert, X509.FORMAT_DER)
         certDN = '/' + '/'.join(x509.get_subject().as_text().split(', '))
     pubkey = x509.get_pubkey().get_rsa()
     verify = EVP.PKey()
@@ -82,9 +82,9 @@ def do_authentication(credentials, env):
     verify.reset_context(cred['hash'])
     verify.verify_init()
     if proxy:
-    	verify.verify_update(proxy)
+        verify.verify_update(proxy)
     else:
-	verify.verify_update(cert)
+        verify.verify_update(cert)
     verify.verify_update(cred['ts'])
     if not verify.verify_final(sign):
         log.info("Signature verification failed")
@@ -93,28 +93,28 @@ def do_authentication(credentials, env):
     ctx = SSL.Context()
     ctx.load_verify_locations(capath="/etc/grid-security/certificates")
     if proxy:
-	log.info("Trying to verify the proxy")
-	if not ctx.validate_certificate(chain):
-		log.info("Certificate verification failed")
-        	raise InvalidCredentials("Certificate verification failed")
+        log.info("Trying to verify the proxy")
+        if not ctx.validate_certificate(chain):
+                log.info("Certificate verification failed")
+                raise InvalidCredentials("Certificate verification failed")
     elif not ctx.validate_certificate(x509):
         log.info("Certificate verification failed")
         raise InvalidCredentials("Certificate verification failed")
     credentials.user_dn = certDN
     if proxy:
-	credentials.dn.append(proxyDN)
+        credentials.dn.append(proxyDN)
     credentials.dn.append(credentials.user_dn)
     if 'SSL_CLIENT_S_DN' in env:
         credentials.dn.append(urllib.unquote_plus(env['SSL_CLIENT_S_DN']))
     if proxy:
-    	voms_client = VomsClient(chain_pem)
-	log.info("proxy path: " + voms_client.proxy_path)
-	fqans = voms_client.get_proxy_fqans()
-	for fqan in fqans:
-		vo = vo_from_fqan(fqan)
-		credentials.voms_cred.append(fqan)
-		if vo not in credentials.vos and vo:
-			credentials.vos.append(vo)
+        voms_client = VomsClient(chain_pem)
+        log.info("proxy path: " + voms_client.proxy_path)
+        fqans = voms_client.get_proxy_fqans()
+        for fqan in fqans:
+                vo = vo_from_fqan(fqan)
+                credentials.voms_cred.append(fqan)
+                if vo not in credentials.vos and vo:
+                        credentials.vos.append(vo)
 
     # Generate the delegation ID
     credentials.delegation_id = generate_delegation_id(credentials.user_dn, credentials.voms_cred)
