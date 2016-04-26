735b517f5df6fa938934bd4f3679e1882ce54169
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2016-04-26 10:45:23 +0200

    FTS-548: Fix for mix of banned/non banned storages

--- src/fts3rest/fts3rest/lib/JobBuilder.py
+++ src/fts3rest/fts3rest/lib/JobBuilder.py
@@ -239,6 +239,9 @@ def _apply_banning(files):
         if timeout is not None:
             f['wait_timestamp'] = now
             f['wait_timeout'] = timeout
+        else:
+            f['wait_timestamp'] = None
+            f['wait_timeout'] = None
 
 
 class JobBuilder(object):
