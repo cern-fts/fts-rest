8611f34ea43a05b9344aab484e40180b4dd36569
Maria Arsuaga Rios <maria.arsuaga.rios@cern.ch>
2018-03-19 15:44:54 +0100

    Indicating job type

--- src/fts3rest/fts3rest/lib/JobBuilder.py
+++ src/fts3rest/fts3rest/lib/JobBuilder.py
@@ -510,8 +510,8 @@ class JobBuilder(object):
         if job_type == 'N' and not self.is_multiple:
             self.job['job_type'] = 'N'
         
-        auto_session_reuse= pylons.config.get('fts3.AutoSessionReuse', 'false');
-        log.debug("AutoSessionReuse "+auto_session_reuse+" and job_type "+job_type)
+        auto_session_reuse= pylons.config.get('fts3.AutoSessionReuse', 'false')
+        log.debug("AutoSessionReuse "+auto_session_reuse+" and job_type "+str(job_type))
         max_size_small_file = pylons.config.get('fts3.AutoSessionReuseMaxFileSize', 104857600) #100MB
         if (auto_session_reuse == 'true' and self.job['source_se'] and self.job['dest_se'] and (job_type is None) and (len(self.files) > 1)) :
             small_files = 0
