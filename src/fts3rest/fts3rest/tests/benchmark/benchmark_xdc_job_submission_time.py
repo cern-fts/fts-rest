551e6740d4147fc0d0d5b1f50d3f3863de233d03
Aris Angelogiannopoulos <aris.angelogiannopoulos@cern.ch>
2019-01-06 11:45:24 +0100

    Add a whoami performance test

--- src/fts3rest/fts3rest/tests/benchmark/benchmark_xdc_job_submission_time.py
+++ src/fts3rest/fts3rest/tests/benchmark/benchmark_xdc_job_submission_time.py
@@ -4,10 +4,11 @@ from optparse import OptionParser
 from util import  *
 
 
-script_path = 'fts-rest-transfer-submit'
+script_transfer_path = '/home/aris/projects/fts-rest/src/cli/fts-rest-transfer-submit'
+script_whoami_path = '/home/aris/projects/fts-rest/src/cli/fts-rest-whoami'
 
 
-def benchmark_auth(opts, auth_method='OAuth2'):
+def benchmark_transfer_auth(opts, auth_method='OAuth2'):
     log.info("Starting benchmark for auth method {0} with {1} files".format(auth_method, str(opts.files_number)))
 
     duration = timedelta()
@@ -19,7 +20,7 @@ def benchmark_auth(opts, auth_method='OAuth2'):
         else:
             options = '--insecure --overwrite -s {0} {1} {2}'.format(opts.fts, src, dst)
         start = datetime.utcnow()
-        var = os.system('python ' + script_path + ' ' + options + ' >/dev/null 2>&1')
+        var = os.system('python ' + script_transfer_path + ' ' + options + ' >/dev/null 2>&1')
         if var != 0:
             log.error("FTS job submission failed")
             exit(-1)
@@ -30,6 +31,25 @@ def benchmark_auth(opts, auth_method='OAuth2'):
     log.info("Total time for {0}:       {1}".format(auth_method, duration_seconds))
     log.info("Files per second for " + auth_method + ": " + str(float(opts.files_number) / duration_seconds))
 
+def benchmark_whoami(opts, auth_method='OAuth2'):
+    log.info("Starting benchmark for whoami with auth method {0}".format(auth_method))
+
+    duration = timedelta()
+    start = datetime.utcnow()
+    for f in xrange(100):
+        if auth_method == 'OAuth2':
+            options = '--insecure --access-token {0} -s {1}'.format(opts.token, opts.fts)
+        else:
+            options = '--insecure --overwrite -s {0}'.format(opts.fts)
+        var = os.system('python ' + script_whoami_path + ' ' + options + ' >/dev/null 2>&1')
+        if var != 0:
+            log.error("FTS whoami query failed")
+            exit(-1)
+    end = datetime.utcnow()
+    duration += (end - start)
+
+    duration_seconds = duration.seconds + (duration.microseconds / 1000000.0)
+    log.info("Total time for {0}:       {1}".format(auth_method, duration_seconds))
 
 if __name__ == "__main__":
     opt_parser = OptionParser()
@@ -43,5 +63,8 @@ if __name__ == "__main__":
     (opts, args) = opt_parser.parse_args()
     log = setup_logging(False)
 
-    benchmark_auth(opts, "OAuth2")
-    benchmark_auth(opts, "X509")
+    benchmark_whoami(opts, "OAuth2")
+    benchmark_whoami(opts, "X509")
+
+    benchmark_transfer_auth(opts, "OAuth2")
+    benchmark_transfer_auth(opts, "X509")
