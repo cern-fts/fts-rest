c8f00fdebf4ff8c054ccf16d7c65036fd836c002
vezhova <viktoriia.ezhova@cern.ch>
2019-11-01 17:00:50 +0100

    Updated

--- src/fts3rest/fts3rest/lib/middleware/fts3auth/methods/oidc.py
+++ src/fts3rest/fts3rest/lib/middleware/fts3auth/methods/oidc.py
@@ -7,6 +7,9 @@ from datetime import datetime, timedelta
 from fts3rest.lib.middleware.fts3auth.credentials import InvalidCredentials, build_vo_from_dn, generate_delegation_id, vo_from_fqan
 from fts3rest.lib.helpers.voms import VomsClient
 
+import os
+import requests
+from requests.exceptions import HTTPError
 
 def do_authentication(credentials, env):
     """
@@ -19,4 +22,13 @@ def do_authentication(credentials, env):
         
     log.info('REMOTE USER '+ env['REMOTE_USER'])
     credentials.user_dn =  env['REMOTE_USER']
+    
+    credentials.vos.append(env['OIDC_CLAIM_email'])
+    #log.info(credentials.vos)
+    
+    for key, value in env.iteritems(): 
+       # log.info(key + ' = ' + value)   
+        if ('OIDC_CLAIM' in key) or ('HTTP_' in key):
+           log.info(key + ' = ' + value)
+    
     return True
