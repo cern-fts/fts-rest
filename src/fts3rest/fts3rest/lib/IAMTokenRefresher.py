2708327800ec57fcd2589498dda1ea789e4699c3
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2019-11-27 14:03:14 +0100

    Fixed missing commit to avoid repeat read

--- src/fts3rest/fts3rest/lib/IAMTokenRefresher.py
+++ src/fts3rest/fts3rest/lib/IAMTokenRefresher.py
@@ -19,6 +19,8 @@ import time
 import random
 from datetime import datetime, timedelta
 from threading import Thread
+from thread import get_ident
+
 
 from fts3rest.lib.base import Session
 from fts3rest.lib.openidconnect import oidc_manager
@@ -50,32 +52,43 @@ class IAMTokenRefresher(Thread):
         self.refresh_interval = int(config.get('fts3.TokenRefreshDaemonIntervalInSeconds', 600))
         self.config = config
 
-    def _thread_not_active(self, thread):
-        # The thread is considered not active if it hasn't updated the DB for 2*refresh_interval
-        return (datetime.utcnow() - thread.beat) > timedelta(seconds=2 * self.refresh_interval)
+    def _thread_is_inactive(self, thread):
+        # The thread is considered inactive if it hasn't updated the DB for 3*refresh_interval
+        log.debug('time since last beat {}'.format(datetime.utcnow() - thread.beat))
+        if (datetime.utcnow() - thread.beat) > timedelta(seconds=3 * self.refresh_interval):
+            log.debug('thread is inactive! taking over, beat {}'.format(thread.beat))
+        return (datetime.utcnow() - thread.beat) > timedelta(seconds=3 * self.refresh_interval)
 
     def run(self):
         """
         Regularly check if there is another active IAMTokenRefresher in the DB. If not, become the active thread.
         """
+        log.debug('CREATE THREAD ID: {}'.format(get_ident()))
+        # Initial sleep in case all threads started at the same time
+        time.sleep(random.randint(0, 60))
         # The interval at which the thread will check if there is another active thread.
         # It is arbitrary: I chose 2 times the refresh interval, plus a random offset to avoid multiple threads
         # checking at the same time (although DB access is transactional)
-        db_check_interval = 2 * self.refresh_interval + random.randint(0, 120)
+        db_check_interval = 3 * self.refresh_interval + random.randint(0, 120)
         while True:
-            # Check that no other fts-token-refresh-daemon is running on this server
+            # Check that no other fts-token-refresh-daemon is running
             refresher_threads = Session.query(Host).filter(Host.service_name == self.tag).all()
-            if all(self._thread_not_active(thread) for thread in refresher_threads):
+            Session.commit()  # Close transaction to avoid repeated read
+            log.debug('refresher_threads {}, ID {}'.format(len(refresher_threads), get_ident()))
+            if all(self._thread_is_inactive(thread) for thread in refresher_threads):
                 log.debug('Activating thread')
                 for thread in refresher_threads:
                     Session.delete(thread)
+                    log.debug('delete thread')
                 host = Host(hostname=socket.getfqdn(), service_name=self.tag)
+                log.debug('host object created')
                 while True:
                     host.beat = datetime.utcnow()
+                    log.debug('THREAD ID: {}, beat {}'.format(get_ident(), host.beat))
                     try:
-                        Session.merge(host)
+                        h2 = Session.merge(host)
                         Session.commit()
-                        log.debug('fts-token-refresh-daemon heartbeat')
+                        log.debug('fts-token-refresh-daemon heartbeat {}'.format(h2.beat))
                     except SQLAlchemyError as ex:
                         log.warning("Failed to update the fts-token-refresh-daemon heartbeat: %s" % str(ex))
                         Session.rollback()
@@ -96,5 +109,6 @@ class IAMTokenRefresher(Thread):
                             raise
                     time.sleep(self.refresh_interval)
             else:
+                log.debug('THREAD ID: {}'.format(get_ident()))
                 log.debug('Another thread is active -- Going to sleep')
                 time.sleep(db_check_interval)
